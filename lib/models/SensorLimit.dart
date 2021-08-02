a37cc5ec72a4289232752fa57efe8b385c53c341
CharlesAMiller <charliea.miller@gmail.com>
2021-08-02 14:28:33 -0700

    Adding model definition for per-enclosure defined limits.

---
+++ lib/models/SensorLimit.dart
@@ -0,0 +1,23 @@
+// Representation of an enclosure's temperature/living conditions.
+//
+//
+class SensorLimit {
+  final double min;
+  final double max;
+  final String sensor_id;
+  final String type;
+
+  SensorLimit(
+      {required this.sensor_id,
+      required this.type,
+      required this.min,
+      required this.max});
+
+  factory SensorLimit.fromJson(Map<String, dynamic> json) {
+    return SensorLimit(
+        sensor_id: json['sensor_id'],
+        type: json['type'],
+        min: json['min'],
+        max: json['max']);
+  }
+}
